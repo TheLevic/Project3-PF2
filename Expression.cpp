afe838e072e285d3d3bfb1592fdec1cdeb77d141
TheLevic <levicrider08@gmail.com>
2021-10-10 15:08:46 -0500

    Added to the expression class

--- Expression.cpp
+++ Expression.cpp
@@ -1,8 +1,30 @@
 #include "Expression.h"
 #include "Token.h"
 
-class Expression{
+Expression::Expression(){
+    original = ""; //Making sure original is Empty
+    type = Bad; //Setting type to bad
+    valid = false; //Setting valid to be false initially
+    tokenized = {}; //Making sure the inital vector is empty
+    postfix = {}; //Making sure the inital vector is empty
+}
+
+Expression::Expression(const string& s){
 
 
-};
+
+
+}
+
+
+
+void Expression::display() const{
+    cout << "original = " << original << endl;
+    //How do I print out the value of tokenized?
+    cout << "number of tokens = " << endl;
+    cout << "postfix = " << endl;
+    cout << "valid = " << valid << endl;
+    cout << "type = " << type << endl;
+    cout << "----------------------------------------" << endl;
+}
 
