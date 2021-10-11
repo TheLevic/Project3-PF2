23fd4092255f4e1d2c68833d3eddab87af311641
TheLevic <levicrider08@gmail.com>
2021-10-11 17:30:46 -0500

    Not great.

--- Expression.cpp
+++ Expression.cpp
@@ -1,8 +1,6 @@
 #include "Expression.h"
 #include "Token.h"
 
-enum SpecialTokens {special = '(' || ')' || '+' || '-' || '*' || '/' || '='}; //Creating a special type to compare to
-
 Expression::Expression(){
     original = ""; //Making sure original is Empty
     type = Bad; //Setting type to bad
@@ -16,15 +14,53 @@ Expression::Expression(const string& s){
 }
 
 void Expression::set(const string& s){
-    original = s;
-    string tmp; //Temporary string to hold values
-    int count = 0;
-    
     //We need to separate each character based on the definition of a token (tokens are separated by spaces or special characters);
-
     //Once the first token is found, add it into tokenized
-    //After adding into tokenized, repeat this process from the position
-    //at which we stopped at until finding the terminating NULL character
+    //After adding into tokenized, repeat this process from the next position
+    //at which we stopped at until finding the terminating NULL character (hence the do while loop)
+    
+    original = s; //Setting original
+    string tmp; //Temporary string to hold values
+    int i = 0;
+    do
+    {
+        
+        while (s[i] != '(' && s[i] != ')' && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/' && s[i] != '=' && s[i] != ' ' && s[i] != NULL){ //If it's an ordinary character
+            tmp =  tmp + s[i]; //Setting tmp = to itself + the next char
+            i++; //moving on
+        }
+
+        while(s[i] == '(' || s[i] == ')' || s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '=' && s[i] != NULL){//If character is special
+            if(tmp.empty() != true){
+                tmp = tmp + ';'; //Adding a semi-colon to the end of tmp for tokenizing purposes.
+                tokenized.push_back(tmp); //Add current value to vector
+                tmp.clear(); //Clear the vector
+                tmp = s[i] + ';'; //Now tmp = a special character + a semi colon.
+                tokenized.push_back(tmp); //Adding tmp to vector
+                tmp.clear(); //Clearing tmp
+                i++; //Moving on
+            }
+        
+        }
+
+        if(s[i] == ' '){ //If s is equal to space
+            if (tmp.empty() != true){ //And if tmp is not empty
+                tmp = tmp + ';'; //Replacing the space with a semi colon
+                tokenized.push_back(tmp); //Adding tmp to our vector
+                tmp.clear(); //clearing tmp
+                i++; //Moving on
+            }
+            else{ //If it is empty
+                i++; //Move on
+            }
+        }
+
+        if(s[i] == NULL){ //If it's a NULL character
+            break; //Break the loops
+        }
+
+
+    } while (s[i] != NULL);
 }
 
 
@@ -35,7 +71,7 @@ void Expression::display() const{
     for (int i = 0; i < tokenized.size(); i++){
        cout << tokenized.at(i).get_token();
     }
-    cout << endl << "number of tokens = " << numTokens << endl;
+    cout << endl << "number of tokens = " << tokenized.size() << endl;
     cout << "postfix = " << endl;
     cout << "valid = " << valid << endl;
     cout << "type = " << type << endl;
--- homework3.cpp
+++ homework3.cpp
@@ -1,12 +1,16 @@
-#include "Token.h"
+//#include "Token.h"
 #include "Expression.h"
 
 int main(){
     string one = "Hello 0234 Good 0234 Friend!";
+    string four = "Hel(lo";
+    string two = "a+b+cde+f";
+    string three = "a12 = 1?ab + - a 0123 c (a + 12 3 )*(ab - (3 + 4 ))";
 
     //Token j(one);
     //j.display();
 
-    Expression object(one);
+    Expression object;
+    object.set(four);
     object.display();
 }
